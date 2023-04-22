b91dc9f9a9c5dc168451e015f33aa0fea69ff3e3
batuhanbilginn <batuhanbilginn@gmail.com>
2023-04-22 16:50:36 +0300

    seo improvements

--- app/post/[slug]/opengraph-image.tsx
+++ app/post/[slug]/opengraph-image.tsx
@@ -7,6 +7,7 @@ export const size = {
   width: 1200,
   height: 630,
 };
+export const alt = "Expolorer | Blog";
 export const contentType = "image/png";
 
 export default async function og({ params }: { params: { slug: string } }) {
